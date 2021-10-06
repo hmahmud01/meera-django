9e8eae661413b745c89cf14fc8905ab1dda230da
Hasan Mahmud <hmahmud01@gmail.com>
2021-10-06 14:31:31 +0600

    product add section done

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,47 @@
+from os import stat_result
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Zone(models.Model):
+    name = models.CharField(max_length=128, null=True, blank=True)
+
+    def __str__(self) :
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_category")
+    price = models.FloatField(null=True, blank=True)
+    disc_price = models.FloatField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    inv_stock = models.IntegerField(null=True, blank=True)    
+    status = models.BooleanField(default=True, null=True, blank=True)    
+    thumb_image = models.FileField('product_thumbnail', upload_to='thumbs', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class ProductZone(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product")
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE, related_name="product_zone")
+
+    def __str__(self):
+        return self.zone.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+    name = models.CharField(max_length=128, null=True, blank=True)
+    photo = models.FileField('product_photo', upload_to='photos', blank=True, null=True)
+
+    def __str__(self):
+        return self.product.name
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.decorators import login_required
+
+from backend.models import Category, Product, ProductImage, ProductZone, Zone
 
 # Create your views here.
 
+@login_required(login_url="/login/")
 def home(request):
     return render(request, "index.html")
 
@@ -11,14 +16,126 @@ def blankpage(request):
 def login(request):
     return render(request, "login.html")
 
+def verifyLogin(request):
+    post_data = request.POST
+
+    if 'username' and 'pass':
+        user = authenticate(
+            request,
+            username = post_data['username'],
+            password = post_data['pass']
+        )
+
+        if user is None:
+            return redirect('login')
+        elif user.is_superuser:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            return redirect('login')
+
+    else:
+        return redirect('login')
+
+def userLogout(request):
+    logout(request)
+    return redirect('login')
+
 def productCreate(request):
-    return render(request, "products/create.html")
+    categories = Category.objects.all()
+    zones = Zone.objects.all()
+    return render(request, "products/create.html", {"categories": categories, "zones": zones})
+
+
+def saveCategory(request):
+    print(request)
+    print(request.POST)
+    data = request.POST
+    category = Category(
+        title = data['title']
+    )
+    category.save()
+    return redirect('productcreate')
+
+def saveZone(request):
+    data = request.POST
+    zone = Zone(
+        name = data['name']
+    )
+    zone.save()
+    return redirect('productcreate')
+
+# <QueryDict: {'csrfmiddlewaretoken': ['NgDZXkPCNkardxwGMHDS9OsLgfGGSsikZvTh0MJwwjqZOMDcqfdCZFgkRv2otH9V'], 
+# 'name': ['Test Name'], 'category': ['op1'], 'price': ['481'], 'disc_price': ['521'], 'inv_stock': ['659'], 'zone': ['z2', 'z3']}>
+
+# <MultiValueDict: {'thumb_image': [<InMemoryUploadedFile: LOGO.png (image/png)>], 
+# 'photos': [<InMemoryUploadedFile: footer.jpg (image/jpeg)>, <InMemoryUploadedFile: footer_2.jpg (image/jpeg)>]}>
+
+def saveProduct(request):
+    post_data = request.POST
+    file_data = request.FILES
+    category = Category.objects.get(id=post_data['category'])
+    
+    product = Product(
+        name=post_data['name'],
+        category=category,
+        price=post_data['price'],
+        disc_price=post_data['disc_price'],
+        description=post_data['description'],
+        inv_stock=post_data['inv_stock'],
+        thumb_image=file_data['thumb_image'],
+    )
+    
+    product.save()
+
+    zones = post_data.getlist("zone")
+    for zone_id in zones:
+        zone = Zone.objects.get(id=zone_id)
+        productzone = ProductZone(
+            product = product,
+            zone = zone
+        )
+        productzone.save()
+
+    photos = file_data.getlist('photos')
+    for photo in photos:
+        name = photo.name.split('/')     
+        image = ProductImage(
+            product = product,
+            name=name,
+            photo = photo
+        )
+        image.save()
+
+    return redirect('productlist')    
+
 
 def productList(request):
-    return render(request, "products/list.html")
+    products = Product.objects.all()
+    return render(request, "products/list.html", {"products": products})
 
-def productDetail(request):
-    return render(request, "products/detail.html")
+def productDetail(request, pid):
+    product = Product.objects.get(id=pid)
+    zones = ProductZone.objects.filter(product_id=pid)
+    images = ProductImage.objects.filter(product_id=pid)
+    return render(request, "products/detail.html", {"product": product, "zones": zones, "images": images})
+
+def statusUpdate(request, state, pid):
+    product = Product.objects.get(id=pid)
+    if state == 'False':
+        product.status = False
+        product.save()
+    else:
+        product.status = True
+        product.save()
+    return redirect('productdetail', pid)
+
+def stockupdate(request, pid):
+    post_data = request.POST
+    product = Product.objects.get(id=pid)
+    product.inv_stock = post_data['stock']
+    product.save()
+    return redirect('productdetail', pid)
 
 def orderList(request):
     return render(request, "orders/index.html")
